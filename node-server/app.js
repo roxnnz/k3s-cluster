25143971d729bf53cedd90ed389b2e99c3dcccfb
Leo LI <xin.leo.li@gmail.com>
2022-12-11 13:31:42 +1300

    init project

---
+++ node-server/app.js
@@ -0,0 +1,18 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/hello-world', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
---
+++ node-server/readme.md
@@ -0,0 +1,4 @@
+This is a vanilla web server build by nodejs and running on port 3000
+
+build image:
+`docker build . -t roxnz/node-web-app` and `docker push roxnz/node-web-app`
